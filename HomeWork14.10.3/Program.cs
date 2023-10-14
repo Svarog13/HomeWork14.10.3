8cbbf1a3a3169d370194268b6b90bda8390e011e
lenovo <lenovo@DESKTOP-D0T6Q1U>
2023-10-14 21:11:02 +0300

    Add project files.

---
+++ HomeWork14.10.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+Console.WriteLine("Enter the digit for diapasos of Fibonacci digit: ");
+int digit = int.Parse(Console.ReadLine());
+
+if (digit <= 0)
+{
+	Console.WriteLine("Please enter the positive digit!");
+}
+else
+{
+	int a = 0, b = 1;
+	for (int i = 0; i < digit; i++)
+	{
+		Console.Write(a + " ");
+		int tmp = a;
+		a = b;
+		b = tmp + b;
+	}
+	Console.WriteLine();
+}
